2a38a77b8ffb7eab412769757c5d7f234ada4dfa
Pedro de Formiga <pedrohborgesborges@gmail.com>
2022-09-02 09:20:15 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/telaum/FormLogin.java
@@ -0,0 +1,25 @@
+package com.example.telaum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class FormLogin extends AppCompatActivity {
+
+    //ImageView logo,appName,splashImg;
+    //LottieAnimationView LottieAnimationView;
+
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+
+        ///logo = findViewById(R.id.logo);
+        //logo = findViewById(R.id.logo);
+        //logo = findViewById(R.id.logo);
+
+    }
+}
